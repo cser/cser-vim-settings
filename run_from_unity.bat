a81dcfb5ee4290a69441d071913ab594efd15d00
sergey_chelnokov <expl@no_mail.ru>
2013-06-16 18:24:25 +0400

    run from unity workarounds for wrapper

---
+++ run_from_unity.bat
@@ -0,0 +1,1 @@
+python %~dp0run_from_unity.py %1
---
+++ run_from_unity.py
@@ -0,0 +1,7 @@
+﻿import sys
+import os
+import time
+
+args = sys.argv[1].split(';')
+print('args[0]=' + args[0])
+os.system('vim' + ' ' + args[0] + ' ++' + args[1]);
